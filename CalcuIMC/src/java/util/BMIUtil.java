8214bea072f3c7e65b7260157637ef1915a7cd92
manoloSKa <47843164+manoloSKa@users.noreply.github.com>
2023-07-21 12:30:18 -0600

    Add files via upload
    Actualizacion

--- CalcuIMC/src/java/util/BMIUtil.java
+++ CalcuIMC/src/java/util/BMIUtil.java
@@ -1,14 +1,16 @@
 package util;
 
 public class BMIUtil {
+    
+    // calculo del indice de masa corporal
     public static double calculateBMI(double height, double weight) {
         return weight / (height * height);
     }
-
+    // filtro para la altura
     public static boolean isValidHeight(double height) {
         return height >= 1 && height <= 2.5;
     }
-
+    // filtro para la edad
     public static boolean isValidAge(int age) {
         return age >= 15;
     }
